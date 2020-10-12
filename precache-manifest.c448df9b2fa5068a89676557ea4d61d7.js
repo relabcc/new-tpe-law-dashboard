367334e8feea1a0619947a5830591b37dd1022e0
Renddi Yeh <renddi@relab.cc>
2020-10-12 10:53:15 +0800

    Updates

---
+++ precache-manifest.c448df9b2fa5068a89676557ea4d61d7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8215ffb5409ec9468e46ba22e1bfaa2d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "897bef8ae78a01d956e8",
+    "url": "./static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "897bef8ae78a01d956e8",
+    "url": "./static/js/2.c974a2a1.chunk.js"
+  },
+  {
+    "revision": "ecbf9a91e960da88c98060e342576b2f",
+    "url": "./static/js/2.c974a2a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4414f5b19d8dccf6c540",
+    "url": "./static/js/main.908f1980.chunk.js"
+  },
+  {
+    "revision": "8f9dd02278e3e65d2062",
+    "url": "./static/js/runtime-main.57d645ad.js"
+  },
+  {
+    "revision": "0df56668096cc4227b5990c1f67acfaf",
+    "url": "./static/media/envelope.0df56668.svg"
+  },
+  {
+    "revision": "bd1cc40fae86b28839110ac27533ce64",
+    "url": "./static/media/listicon.bd1cc40f.svg"
+  }
+]);
